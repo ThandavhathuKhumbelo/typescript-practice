7f04acc8931a3870e1f2ac49d53f84b32ca48181
Thandavhathu Khumbelo <thandakhumbe@gmail.com>
2026-03-12 13:42:23 +0200

    Add files via upload

---
+++ interface.ts
@@ -0,0 +1,8 @@
+let balance: number = 1000
+let withdrawal: number = 800
+
+if (balance > withdrawal) {
+    console.log ('successful withdrwal');
+} else {
+    console.log ('transcation declined: insufficient funds')
+}
