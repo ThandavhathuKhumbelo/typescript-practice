131a66f509e543e854ca61d34c0e6bccb0ce5bea
Thandavhathu Khumbelo <thandakhumbe@gmail.com>
2026-03-16 09:19:37 +0200

    Add files via upload

---
+++ aliases1.ts
@@ -0,0 +1,14 @@
+
+type Car = {
+    carName : string;
+    carYear : number;
+    carModel : string;
+};
+
+const car1: Car ={
+    carName : 'Benz',
+    carYear: 2005,
+    carModel: 'Z550G'
+
+};
+console.log('name of car is '+ car1.carName + 'and car year is ' + car1.carYear)
