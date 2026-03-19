d4cb14a33feb3e07b570ed26b5e54ddeefd7b607
Thandavhathu Khumbelo <thandakhumbe@gmail.com>
2026-03-19 16:27:22 +0200

    Add files via upload

---
+++ 3_data_types.ts
@@ -0,0 +1,7 @@
+// nextPaymentDate.ts
+let paymentDate1: Date = new Date ("2026-03-11");
+let walletID: string = "WLT-015";
+let paymentAmount: number = 700
+let currency: string = "ZAR";
+
+console.log("your payment is due on: "+ (paymentDate1) + "to walletID" + (walletID)  + (currency) + "with am amount of" + (paymentAmount)); 
